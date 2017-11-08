d215819e2105a9b0cb876022452da7ae60194d39
Adam Wittnam <adam.wittnam@hotmail.com>
2017-11-08 14:06:02 -0800

    framework

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var userName = prompt('What is you\'re name?')
+console.log('user\'s name:', userName)
+  alert('Hello ' + userName + ', it\'s nice to meet you.  In order for you to get to know me a little bit we are going to play a guessing game.  Let\'s get started! ')
+
+
+
+
+
+
+
+// var kids = prompt('how many kids do you have?');
+// console.log('number of kids:', kids);
+//
+// var hats = prompt('how many hats do you own?');
+// console.log('number of hats:', hats);
+//
+// if (kids === '4' || kids === '3') {
+//   alert('nice job - u right');
+// }
+//
+// if (hats === '3' && kids === '4') {
+//   alert('nice job again - u rule');
+// }
